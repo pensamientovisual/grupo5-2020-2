f6516ff8d19cc60d1704bc5c893f7f37ea598901
RODRIGO JIMENEZ VIDAL <r.jimenezvidal@uc.cl>
2020-10-11 16:29:13 -0300

    main app

--- index_java.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function (){
-    $(".texto_def").hide();
-    $("#definiciones").mouseenter(function (){
-        $(".texto_def").show();
-    });
-    $("#definiciones").mouseleave(function (){
-        $(".texto_def").hide();
-    });
-});
