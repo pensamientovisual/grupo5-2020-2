138dbfa11b10f63e2db6c61ecabe04a31a0927d5
rominabakulic <romina.bakulic@uc.cl>
2020-10-12 09:16:57 -0300

    mapa casi listo

--- mapa.js
+++ mapa.js
@@ -9,18 +9,18 @@ $(document).ready(function(){
     $(".click8").hide();
     $(".click9").hide();
     $(".click10").hide();
-    $("#I1").hide();
-    $("#I2").hide();
-    $("#I3").hide();
-    $("#I4").hide();
-    $("#I5").hide();
-    $("#I6").hide();
-    $("#I7").hide();
+    $(".imagenes1").hide();
+    $(".imagenes2").hide();
+    $(".imagenes3").hide();
+    $(".imagenes4").hide();
+    $(".imagenes5").hide();
+
+
     var regiones = $(".region");
     regiones.mouseenter(function(){
-        var idregion = $(this).attr("title");
-        $("#titulo_mapa").text(idregion);
-        $(this).fadeTo("fast", 0.5);
+        var nombreregion = $(this).attr("title");
+        $("#titulo_mapa").text(nombreregion);
+        $(this).fadeTo("fast", 0.5)
     });
     regiones.mouseleave(function(){
         $("#titulo").text("Mapa de Chile");
@@ -31,24 +31,12 @@ $(document).ready(function(){
         if (escondido1==true&&año1==true){
             $(".click1").show();
             $(".click2").show();
-            $("#I1").show();
-            $("#I2").show();
-            $("#I3").show();
-            $("#I4").show();
-            $("#I5").show();
-            $("#I6").show();
-            $("#I7").hide();
+            $(".imagenes1").show();
             escondido1 = false}
         else{
             $(".click1").hide();
             $(".click2").hide();
-            $("#I1").hide();
-            $("#I2").hide();
-            $("#I3").hide();
-            $("#I4").hide();
-            $("#I5").hide();
-            $("#I6").hide();
-            $("#I7").hide();
+            $(".imagenes1").hide();
             escondido1 = true}
     });
     var escondido2 = true;
@@ -56,24 +44,12 @@ $(document).ready(function(){
         if (escondido2==true&&año2==true){
             $(".click3").show();
             $(".click4").show();
-            $("#I1").show();
-            $("#I2").show();
-            $("#I3").show();
-            $("#I4").show();
-            $("#I5").show();
-            $("#I6").show();
-            $("#I7").show();
+            $(".imagenes2").show();
             escondido2 = false}
         else{
             $(".click3").hide();
             $(".click4").hide();
-            $("#I1").hide();
-            $("#I2").hide();
-            $("#I3").hide();
-            $("#I4").hide();
-            $("#I5").hide();
-            $("#I6").hide();
-            $("#I7").hide();
+            $(".imagenes2").hide();
             escondido2 = true}
     });
     var escondido3 = true;
@@ -81,14 +57,12 @@ $(document).ready(function(){
         if (escondido3==true&&año3==true){
             $(".click5").show();
             $(".click6").show();
-            $(".imagenes").show();
-            $("#I6").hide();
-            $("#I7").hide();
+            $(".imagenes3").show();
             escondido3 = false}
         else{
             $(".click5").hide();
             $(".click6").hide();
-            $(".imagenes").hide();
+            $(".imagenes3").hide();
             escondido3 = true}
     });
     var escondido4 = true;
@@ -96,24 +70,12 @@ $(document).ready(function(){
         if (escondido4==true&&año4==true){
             $(".click7").show();
             $(".click8").show();
-            $("#I1").show();
-            $("#I2").show();
-            $("#I3").show();
-            $("#I4").show();
-            $("#I5").show();
-            $("#I6").show();
-            $("#I7").show();
+            $(".imagenes4").show();
             escondido4 = false}
         else{
             $(".click7").hide();
             $(".click8").hide();
-            $("#I1").hide();
-            $("#I2").hide();
-            $("#I3").hide();
-            $("#I4").hide();
-            $("#I5").hide();
-            $("#I6").hide();
-            $("#I7").hide();
+            $(".imagenes4").hide();
             escondido4 = true}
     });
     var escondido5 = true;
@@ -121,25 +83,13 @@ $(document).ready(function(){
         if (escondido5==true&&año5==true){
                 $(".click9").show();
                 $(".click10").show();
-                $("#I1").show();
-                $("#I2").show();
-                $("#I3").show();
-                $("#I4").show();
-                $("#I5").show();
-                $("#I6").show();
-                $("#I7").show();
-                escondido2 = false}
+                $(".imagenes5").show();
+                escondido5 = false}
             else{
                 $(".click9").hide();
                 $(".click10").hide();
-                $("#I1").hide();
-                $("#I2").hide();
-                $("#I3").hide();
-                $("#I4").hide();
-                $("#I5").hide();
-                $("#I6").hide();
-                $("#I7").hide();
-                escondido2 = true}
+                $(".imagenes5").hide();
+                escondido5 = true}
             });
     var año1= false;
     var año2= false;
